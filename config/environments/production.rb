4fd0d113796493892130460b3426258cb8c71996
morris menanya <emmiemenz@gmail.com>
2024-09-25 05:11:16 +0100

    aff server url for images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 
 
 Rails.application.routes.default_url_options = {
-  host: "http://localhost:3000"
+  host: "https://mellifera-quiz-review-38fe23334e2d.herokuapp.com"
 }
 
 Rails.application.configure do
